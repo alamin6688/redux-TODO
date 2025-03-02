a5f90171608301e61ce35f02b9278625297cea23
alamin6688 <alamin.kzs06@gmail.com>
2025-03-02 23:21:59 +0600

    BaseApi connected

--- src/pages/tasks.tsx
+++ src/pages/tasks.tsx
@@ -1,28 +1,17 @@
 import { AddTaskModal } from "@/components/module/tasks/addTaskModal";
-// import TaskCard from "@/components/module/tasks/taskCard";
+import TaskCard from "@/components/module/tasks/taskCard";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useGetTasksQuery } from "@/redux/api/baseApi";
+import { ITask } from "@/types";
 
 const Tasks = () => {
-  // const tasks = [
-  //   {
-  //     id: "1",
-  //     title: "Task 1",
-  //     description: "This is a task",
-  //     priority: "low",
-  //   },
-  //   {
-  //     id: "2",
-  //     title: "Task 2",
-  //     description: "This is a task",
-  //     priority: "medium",
-  //   },
-  //   {
-  //     id: "3",
-  //     title: "Task 3",
-  //     description: "This is a task",
-  //     priority: "high",
-  //   },
-  // ];
+  const { data, isLoading } = useGetTasksQuery(undefined);
+
+  console.log({ data, isLoading });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="w-full mx-auto">
@@ -30,38 +19,19 @@ const Tasks = () => {
         <h1 className="text-center mr-auto text-3xl">Tasks</h1>
         <Tabs defaultValue="all">
           <TabsList className="grid w-full grid-cols-4">
-            <TabsTrigger
-              // onClick={() => dispatch(updateFilter("all"))}
-              value="all"
-            >
-              All
-            </TabsTrigger>
-            <TabsTrigger
-              // onClick={() => dispatch(updateFilter("low"))}
-              value="low"
-            >
-              Low
-            </TabsTrigger>
-            <TabsTrigger
-              // onClick={() => dispatch(updateFilter("medium"))}
-              value="medium"
-            >
-              Medium
-            </TabsTrigger>
-            <TabsTrigger
-              // onClick={() => dispatch(updateFilter("high"))}
-              value="high"
-            >
-              High
-            </TabsTrigger>
+            <TabsTrigger value="all">All</TabsTrigger>
+            <TabsTrigger value="low">Low</TabsTrigger>
+            <TabsTrigger value="medium">Medium</TabsTrigger>
+            <TabsTrigger value="high">High</TabsTrigger>
           </TabsList>
         </Tabs>
         <AddTaskModal />
       </div>
       <div className="space-y-5 mt-4">
-        {/* {tasks.map((task) => {
-          return <TaskCard tasks={task} key={task.id} />;
-        })} */}
+        {!isLoading &&
+          data.tasks.map((task: ITask) => {
+            return <TaskCard task={task} key={task.id} />;
+          })}
       </div>
     </div>
   );
