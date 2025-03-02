3d0f324b079663fc5ef07e1b83114bf48c688c4d
alamin6688 <alamin.kzs06@gmail.com>
2025-03-02 15:29:33 +0600

    RTK Query setup done

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./features/counter/counterSlice";
-import logger from "./middlewars/logger";
-import taskReducer from "./features/counter/task/taskSlice";
-import userReducer from "./features/counter/user/userSlice";
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    todo: taskReducer,
-    user: userReducer,
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  reducer: {},
 });
 
 export type RootState = ReturnType<typeof store.getState>;
