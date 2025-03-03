47349c8fd5e321b801d41aeb415de7af026a0c9b
alamin6688 <alamin.kzs06@gmail.com>
2025-03-03 06:45:58 +0600

    Caching data managed

--- src/pages/tasks.tsx
+++ src/pages/tasks.tsx
@@ -5,7 +5,12 @@ import { useGetTasksQuery } from "@/redux/api/baseApi";
 import { ITask } from "@/types";
 
 const Tasks = () => {
-  const { data, isLoading } = useGetTasksQuery(undefined);
+  const { data, isLoading } = useGetTasksQuery(undefined, {
+    // pollingInterval: 30000,
+    refetchOnFocus: true,
+    refetchOnMountOrArgChange: true,
+    refetchOnReconnect: true,
+  });
 
   console.log({ data, isLoading });
 
